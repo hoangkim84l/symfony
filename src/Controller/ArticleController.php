d49bd4927f3596a1172e545c6cbe6d2ee8beb30d
vpduy <vd@embrace-it.com>
2020-01-16 17:46:38 +0700

    adding template twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 
-class ArticleController{
+class ArticleController extends AbstractController{
     /**
      * @Route("/")
     */
